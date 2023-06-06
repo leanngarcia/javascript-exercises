413812c2fdf062ac9a1832e0a9a50c3151770b0a
Lean <leanndro.garcia@gmail.com>
2023-06-05 22:00:08 -0300

    ejercicio 4 resuelto

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, item) {
+  let nuevoArray = [];
 
+  for (let i = 1; i < arguments.length; i++) {
+    console.log(arguments[i]);
+
+    if (array.indexOf(arguments[i]) >= 0) {
+      array.splice(array.indexOf(arguments[i]), 1);
+    }
+  }
+
+  return array;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
