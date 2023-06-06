538481f026398ad0a656cc8f26f28f691cfa1c96
Lean <leanndro.garcia@gmail.com>
2023-06-05 21:00:32 -0300

    ejercicio 2 resuelto

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, number) {
+  let word = string;
 
+  if (number < 0) return "ERROR";
+
+  if (number === 0) return "";
+
+  for (let i = 1; i < number; i++) {
+    string = string + word;
+  }
+
+  return string;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
