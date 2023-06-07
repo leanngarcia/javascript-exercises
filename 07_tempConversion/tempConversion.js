d7c3b4fc95036165672e923e976256f3dabe758a
Lean <leanndro.garcia@gmail.com>
2023-06-07 00:54:14 -0300

    ejercicio 7 resuelto

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (farenheitTemp) {
+  let celciusTemp = (farenheitTemp - 32) * (5 / 9);
+
+  celciusTemp = Math.round(celciusTemp * 10) / 10;
+
+  return celciusTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celciusTemp) {
+  let farenheitTemp = (celciusTemp * (9 / 5)) + 32;
+
+  farenheitTemp = Math.round(farenheitTemp * 10) / 10;
+  return farenheitTemp;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
