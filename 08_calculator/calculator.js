0ce582a8ec70d05093c5e6f2ab2324d646f92ecf
lean <leanndro.garcia@gmail.com>
2023-08-18 20:38:25 -0300

    exercise 8 resolverd

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sum = 0;
+  for (const number of arr) {
+    sum += number;
+  }
+
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let mult = 1;
+  for (const number of arr) {
+    mult *= number;
+  }
+  return mult;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let mult = 1;
+  for (i = 1; i <= num; i++) {
+    mult *= i;
+  }
+  return mult;
 };
 
 // Do not edit below this line
@@ -29,5 +42,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
